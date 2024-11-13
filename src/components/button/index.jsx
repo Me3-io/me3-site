dd5a4851cb1f33a4c9076c2969ca2f2eaa44cf2d
eveiasenza <eveiasenza@7r1ck.com>
2024-11-13 12:46:55 -0300

    change href

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,17 +1,18 @@
 import styled from "./styled.module.scss";
 
 export const ButtonDefault = ({ href, text }) => {
+  const link = typeof href === "string" ? href : href?.url || "#";
   return (
-    <a className={styled.buttonDefault} href={href} target="_blank" rel="noreferrer">
+    <a className={styled.buttonDefault} href={link} target="_blank" rel="noreferrer">
       <p>{text}</p>
     </a>
   );
 };
 
 export const ButtonAnimated = ({ href, text }) => {
-  const validHref = typeof href === "string" ? href : "#";
+  const link = typeof href === "string" ? href : href?.url || "#";
   return (
-    <a className={styled.button} href={validHref} target="_blank" rel="noreferrer">
+    <a className={styled.button} href={link} target="_blank" rel="noreferrer">
       <span></span>
       <span></span>
       <span></span>
